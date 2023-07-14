20c8dcdb55ea47b7f5b0369b724966459712aaae
7thclass <msai@live.co.uk>
2023-07-14 14:05:23 +0530

    horizontal overflow disabled

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
  return (
   <Html lang="en">
    <Head />
-   <body className="relative" id="body">
+   <body className="relative overflow-x-hidden" id="body">
     <Main />
     <NextScript />
    </body>
