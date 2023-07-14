2abcd9eb741d24d159cb669a292abd66d64e90b6
7thclass <msai@live.co.uk>
2023-07-14 14:02:35 +0530

    footer bg changed according to spec

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import Logo from "../Hero/assets/Logo";
 
 const Footer: React.FC = () => {
  return (
-  <footer className="h-[135px] w-screen flex flex-col justify-between p-10 items-center md:flex-row mt-10 bg-pink-700">
+  <footer className="h-[135px] w-screen flex flex-col justify-between p-10 items-center md:flex-row mt-10 bg-formBg">
    <Logo color="#A3ACAC" height={20} width={90} />
    <p className="text-l" style={{ color: "#A3ACAC" }}>
     © 2023 Multor Enterprises. All rights reserved.
